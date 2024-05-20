f92cfc7569d9a7e26d6934cb4032f4f801baa138
bernardo <bernardo.alemar@hotmail.com>
2024-05-20 11:55:55 -0300

    Facahda para bibliotecas externas

---
+++ src/fachada/calculation.py
@@ -0,0 +1,22 @@
+import numpy as np
+from typing import List
+
+class __Calculation:
+    def __init__(self) -> None:
+        self.__np = np
+
+    def random_real_number(self,number_min:int,number_max:int) -> int:
+        """
+        Escolhe número aleatório entre os range max e min
+        number_min: Range Mínimo do Número
+        number_max: Range Máximo do Número
+        """
+        random_number = self.__np.random.randint(number_min,number_max)
+        return random_number
+
+    def average(self,numbers:List) -> float:
+        average_number = self.__np.average(numbers)
+        return average_number
+
+
+calculation = __Calculation()
