871b601e33d62d4de97aaff6957ba6647b878c9d
bernardo <bernardo.alemar@hotmail.com>
2024-05-22 10:34:12 -0300

    pytest calculadora 3

---
+++ src/tests/calculadora_3_controller_test.py
@@ -0,0 +1,19 @@
+from faker import Faker
+from src.controllers.calculadora_3_controller import Calculadora_3
+
+
+fake = Faker()
+
+
+def test_calculadora_1():
+    random_list_numbers = [fake.random_int() for _ in range(fake.random_int())]
+    random_list_numbers = str(random_list_numbers)[1:-1]
+    calculadora_3 = Calculadora_3()
+    
+    expect = True
+
+    response = calculadora_3.calculations(random_list_numbers)
+    result = response['success']
+    assert expect == result
+
+
