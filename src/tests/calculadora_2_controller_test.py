6e592f075b93ad06d7ad0fb92a04183d9baf069c
bernardo <bernardo.alemar@hotmail.com>
2024-05-22 10:37:39 -0300

    consertando nomes pytest

--- src/tests/calculadora_2_controller_test.py
+++ src/tests/calculadora_2_controller_test.py
@@ -5,7 +5,7 @@ from src.controllers.calculadora_2_controller import Calculadora_2
 fake = Faker()
 
 
-def test_calculadora_1():
+def test_calculadora_2():
     random_list_numbers = [fake.random_int() for _ in range(fake.random_int())]
     random_list_numbers = str(random_list_numbers)[1:-1]
     calculadora_2 = Calculadora_2()
--- src/tests/calculadora_3_controller_test.py
+++ src/tests/calculadora_3_controller_test.py
@@ -5,7 +5,7 @@ from src.controllers.calculadora_3_controller import Calculadora_3
 fake = Faker()
 
 
-def test_calculadora_1():
+def test_calculadora_3():
     random_list_numbers = [fake.random_int() for _ in range(fake.random_int())]
     random_list_numbers = str(random_list_numbers)[1:-1]
     calculadora_3 = Calculadora_3()
