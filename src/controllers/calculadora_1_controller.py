4a520cd02c2c12c788f82ce0035f4199d9cb6a97
bernardo <bernardo.alemar@hotmail.com>
2024-06-05 11:02:54 -0300

    Consertando cod implementando metodos privados

--- src/controllers/calculadora_1_controller.py
+++ src/controllers/calculadora_1_controller.py
@@ -12,8 +12,6 @@ class Calculadora_1:
     def __init__(self) -> None:
         # Comum calculations
         self.nome = "Calculadora 1"
-        
-
     
     def calculations(self,number:float) -> Dict:
         try:
@@ -23,9 +21,9 @@ class Calculadora_1:
 
             self.number = number//3
             
-            resultado_1 = Calculadora_1_parte_1(self.number).run()
+            resultado_1 = self.__calculadora_parte_1(self.number)
             
-            resultado_2 = Calculadora_1_parte_2(self.number).run()
+            resultado_2 = self.__calculadora_parte_2(self.number)
 
             resultado_3 = self.number
             
@@ -37,6 +35,16 @@ class Calculadora_1:
             return {"success":False,"error":str(e)}
 
 
+    def __calculadora_parte_1(number):
+        number = (number/4) + 7
+        number = math.sqrt(number) * 0.257
+        return number
+    
+    def __calculadora_parte_2(number):
+        number = number ** 2.121
+        number = (number / 5) + 1
+        return number
+    
     def __format_response(self,result:List) -> Dict:
         # Isso deveria ficar na view?
         return {
@@ -51,33 +59,38 @@ class Calculadora_1:
 
 
 
-class Calculadora_1_parte_1:
-    def __init__(self,number) -> None:
-        self.number = number
+# Corrigir e botar esses classes como metódos privados dentro de Calculadora_1()
+# Processamento só em um método privado
 
-    def run(self):
-        self.__parte_1()
-        self.__parte_2()
-        return self.number
+# Nao vai alterando valor com self. isso da merda
+# class Calculadora_1_parte_1:
+#     def __init__(self,number) -> None:
+#         self.number = number
 
-    def __parte_1(self):
-        self.number = (self.number/4) + 7
-
-    def __parte_2(self):
-        self.number = math.sqrt(self.number) * 0.257
-
-
-class Calculadora_1_parte_2:
-    def __init__(self,number) -> None:
-        self.number = number
+#     def run(self):
+#         self.__parte_1()
+#         self.__parte_2()
+#         return self.number
     
-    def run(self):
-        self.__parte_1()
-        self.__parte_2()
-        return self.number
+#     # isso virará variável
+#     def __parte_1(self):
+#         self.number = (self.number/4) + 7
+
+#     def __parte_2(self):
+#         self.number = math.sqrt(self.number) * 0.257
+
+
+# class Calculadora_1_parte_2:
+#     def __init__(self,number) -> None:
+#         self.number = number
     
-    def __parte_1(self):
-        self.number = self.number ** 2.121
-    def __parte_2(self):
-        self.number = (self.number/5) + 1
+#     def run(self):
+#         self.__parte_1()
+#         self.__parte_2()
+#         return self.number
+    
+#     def __parte_1(self):
+#         self.number = self.number ** 2.121
+#     def __parte_2(self):
+#         self.number = (self.number/5) + 1
 
--- src/controllers/calculadora_3_controller.py
+++ src/controllers/calculadora_3_controller.py
@@ -9,9 +9,8 @@ class Calculadora_3:
         try:
             self.entry_number = list_numbers
 
-            variance,standard_deviation = calculo(list_numbers).run()
+            variance,standard_deviation = self.__calculo(self.entry_number)
             result = self.__compare_variance_std(variance,standard_deviation)
-
             self.variance = variance
             self.std = standard_deviation
             return self.__format_response(result)
@@ -25,6 +24,16 @@ class Calculadora_3:
         else:
             return "Varianca Menor"
     
+    def __calculo(self,list_numbers) -> Tuple[float,float]:
+        list_numbers = self.__fixing_list_number(list_numbers)
+        variance  =  math_operations.variance(list_numbers)
+        standard_deviation = math_operations.standard_deviation(list_numbers)
+        return [variance,standard_deviation]
+
+    def __fixing_list_number(self,list_numbers) -> List:
+        list_numbers = list_numbers.split(',')
+        list_numbers = [float(x.strip()) for x in list_numbers]
+
     def __format_response(self,result:List) -> Dict:
         # Isso deveria ficar na view?
         return {
@@ -39,19 +48,21 @@ class Calculadora_3:
             }
         }
 
-class calculo:
-    def __init__(self,list_numbers:List) -> None:
-        self.list_numbers = list_numbers
-        self.__fixing_list_number()
+# Método público tipo constructor e lógica no método privado...
+# Deixar como métodos privados
+# class calculo:
+#     def __init__(self,list_numbers:List) -> None:
+#         self.list_numbers = list_numbers
+#         self.__fixing_list_number()
     
-    def run(self):
-        return self.__calculo_1()
+#     def run(self):
+#         return self.__calculo_1()
 
-    def __calculo_1(self) -> Tuple[float,float]:
-        variance  =  math_operations.variance(self.list_numbers)
-        standard_deviation = math_operations.standard_deviation(self.list_numbers)
-        return [variance,standard_deviation]
+#     def __calculo_1(self) -> Tuple[float,float]:
+#         variance  =  math_operations.variance(self.list_numbers)
+#         standard_deviation = math_operations.standard_deviation(self.list_numbers)
+#         return [variance,standard_deviation]
 
-    def __fixing_list_number(self) -> List:
-        self.list_numbers = self.list_numbers.split(',')
-        self.list_numbers = [float(x.strip()) for x in self.list_numbers]
+#     def __fixing_list_number(self) -> List:
+#         self.list_numbers = self.list_numbers.split(',')
+#         self.list_numbers = [float(x.strip()) for x in self.list_numbers]
